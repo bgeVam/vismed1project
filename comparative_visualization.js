b3eee83ecbc616b4ff1651ab14a7bd8ec46693c6
CarolineMagg <caroline_magg@hotmail.com>
2018-05-24 10:35:20 +0200

    Update comparative_visualization.js

--- comparative_visualization.js
+++ comparative_visualization.js
@@ -1,46 +1,46 @@
 /* globals dat, AMI*/
 
 // Setup renderer left
-var container_left = document.getElementById('container_left');
-var renderer_left = new THREE.WebGLRenderer({
+var containerLeft = document.getElementById('container_left');
+var rendererLeft = new THREE.WebGLRenderer({
     antialias: true
 });
-renderer_left.setSize(container_left.offsetWidth, container_left.offsetHeight);
-renderer_left.setClearColor(0x353535, 1);
-renderer_left.setPixelRatio(window.devicePixelRatio);
-container_left.appendChild(renderer_left.domElement);
+rendererLeft.setSize(containerLeft.offsetWidth, containerLeft.offsetHeight);
+rendererLeft.setClearColor(0x353535, 1);
+rendererLeft.setPixelRatio(window.devicePixelRatio);
+containerLeft.appendChild(rendererLeft.domElement);
 
-// Setup renderer left
-var container_right = document.getElementById('container_right');
-var renderer_right = new THREE.WebGLRenderer({
+// Setup renderer right
+var containerRight = document.getElementById('container_right');
+var rendererRight = new THREE.WebGLRenderer({
     antialias: true
 });
-renderer_right.setSize(container_right.offsetWidth, container_right.offsetHeight);
-renderer_right.setClearColor(0x353535, 1);
-renderer_right.setPixelRatio(window.devicePixelRatio);
-container_right.appendChild(renderer_right.domElement);
+rendererRight.setSize(containerRight.offsetWidth, containerRight.offsetHeight);
+rendererRight.setClearColor(0x353535, 1);
+rendererRight.setPixelRatio(window.devicePixelRatio);
+containerRight.appendChild(rendererRight.domElement);
 
 // Setup scene
 var scene = new THREE.Scene();
 
 // Setup camera
-var camera = new THREE.PerspectiveCamera(45, container_left.offsetWidth / container_left.offsetHeight, 0.01, 10000000);
+var camera = new THREE.PerspectiveCamera(45, containerLeft.offsetWidth / containerLeft.offsetHeight, 0.01, 10000000);
 camera.position.x = 150;
 camera.position.y = 150;
 camera.position.z = 100;
 
 // Setup controls
-var controls_left = new AMI.TrackballControl(camera, container_left);
-var controls_right = new AMI.TrackballControl(camera, container_right);
+var controlsLeft = new AMI.TrackballControl(camera, containerLeft);
+var controlsRight = new AMI.TrackballControl(camera, containerRight);
 /**
  * Handle window resize
  */
 function onWindowResize() {
-    camera.aspect = container_left.offsetWidth / container_left.offsetHeight;
+    camera.aspect = containerLeft.offsetWidth / containerLeft.offsetHeight;
     camera.updateProjectionMatrix();
 
-    renderer_left.setSize(container_left.offsetWidth, container_left.offsetHeight);
-    renderer_right.setSize(container_right.offsetWidth, container_right.offsetHeight);
+    rendererLeft.setSize(containerLeft.offsetWidth, containerLeft.offsetHeight);
+    rendererRight.setSize(containerRight.offsetWidth, containerRight.offsetHeight);
 }
 
 window.addEventListener('resize', onWindowResize, false);
@@ -105,10 +105,10 @@ function gui(stackHelper) {
  * Start animation loop
  */
 function animate() {
-    controls_left.update();
-    controls_right.update();
-    renderer_left.render(scene, camera);
-    renderer_right.render(scene, camera);
+    controlsLeft.update();
+    controlsRight.update();
+    rendererLeft.render(scene, camera);
+    rendererRight.render(scene, camera);
     // request new frame
     requestAnimationFrame(function() {
         animate();
@@ -117,8 +117,8 @@ function animate() {
 animate();
 
 // Setup loader
-var loader_left = new AMI.VolumeLoader(container_left);
-var loader_right = new AMI.VolumeLoader(container_right);
+var loaderLeft = new AMI.VolumeLoader(containerLeft);
+var loaderRight = new AMI.VolumeLoader(containerRight);
 var t2 = [
     '36444280',
     '36444294',
@@ -144,14 +144,14 @@ var files = t2.map(function(v) {
     return 'https://cdn.rawgit.com/FNNDSC/data/master/dicom/adi_brain/' + v;
 });
 
-loader_left
+loaderLeft
     .load(files)
     .then(function() {
         // merge files into clean series/stack/frame structure
-        var series = loader_left.data[0].mergeSeries(loader_left.data);
+        var series = loaderLeft.data[0].mergeSeries(loaderLeft.data);
         var stack = series[0].stack[0];
-        loader_left.free();
-        loader_left = null;
+        loaderLeft.free();
+        loaderLeft = null;
         // be carefull that series and target stack exist!
         var stackHelper = new AMI.StackHelper(stack);
         stackHelper.bbox.color = 0x8bc34a;
@@ -166,21 +166,21 @@ loader_left
         var centerLPS = stackHelper.stack.worldCenter();
         camera.lookAt(centerLPS.x, centerLPS.y, centerLPS.z);
         camera.updateProjectionMatrix();
-        controls_left.target.set(centerLPS.x, centerLPS.y, centerLPS.z);
+        controlsLeft.target.set(centerLPS.x, centerLPS.y, centerLPS.z);
     })
     .catch(function(error) {
         window.console.log('oops... something went wrong...');
         window.console.log(error);
     });
 
-loader_right
+loaderRight
     .load(files)
     .then(function() {
         // merge files into clean series/stack/frame structure
-        var series = loader_right.data[0].mergeSeries(loader_right.data);
+        var series = loaderRight.data[0].mergeSeries(loaderRight.data);
         var stack = series[0].stack[0];
-        loader_right.free();
-        loader_right = null;
+        loaderRight.free();
+        loaderRight = null;
     })
     .catch(function(error) {
         window.console.log('oops... something went wrong...');
